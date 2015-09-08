2fea4a3f959831d9acb23597f5d475383807b383
Eric Newton <eric.newton@lightmaker.com>
2015-09-08 10:17:26 -0400

    initial commit of src

---
+++ src/HttpCookieManager/HttpCookieManager/CookieManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpCookieManager
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Web;
+
+	namespace HttpCookieManager
+	{
+
+		public class CookieManager
+		{
+			private static CookieManager S_CookieManager = new CookieManager();
+			public static CookieManager Current { get { return S_CookieManager; } }
+
+			public virtual bool Exists<T>(HttpContextBase httpContextBase)
+			{
+				throw new NotImplementedException();
+			}
+			public virtual void SetCookie<T>(HttpResponseBase httpReponse, T value)
+			{
+				throw new NotImplementedException();
+			}
+			public virtual T GetCookie<T>(HttpRequestBase httpRequest)
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+
+}
